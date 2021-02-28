036f4f5ae0c5b8e29b0e56411b9345a4e4935b26
Nihal Ansari <nihalansari@googlemail.com>
2021-02-28 15:21:30 +0800

    session save

--- routes/products.js
+++ routes/products.js
@@ -6,10 +6,14 @@ const inventoryDataset = './data/inventory.json';
 const locationDataset = './data/location.json';
 
 router.route('/').get((req, res) => {
-    res.send(queryClassDetails(dataset));
+    const queryObject = url.parse(req.url,true).query;
+    console.log(queryObject.productname);
+    console.log(queryObject.suburb);
+    res.send(queryProductStock(inventoryDataset,queryObject.productname,queryObject.suburb));
     res.end();
 });
 
+/*
 router.route('/stock').get((req, res) => {
     const queryObject = url.parse(req.url,true).query;
     console.log(queryObject.courseid);
@@ -30,130 +34,39 @@ router.route('/queryAllStores').get((req, res) => {
     res.send(queryAllStudents(dataset));
     res.end();
 });
+*/
 
 var myHashTable = {};
 
-let queryAllCourses = function(){
-    var bufferAsJson = [];
-    for(key in myHashTable){
-        bufferAsJson.push(key);
-    }
-    return bufferAsJson;
-}
-let queryAllStudents = function(){
-    return {};
-}
-
-
-let queryClassDetails = function(dataset) {
+let queryProductStock = function(inventoryDataset, prodname, suburb) {
     const fs = require('fs');
-    let fileAsStr = fs.readFileSync(dataset).toString();
+    let fileAsStr = fs.readFileSync(inventoryDataset).toString();
     let fileAsJson = JSON.parse(fileAsStr);
     myHashTable = {};
-
+    var prodfound = false;
+    var subfound = false;
 //Step1: identify unique courses. Append all student names against the subject_code
     for(i of fileAsJson){
-        for(j of i.class_details){
-            //Add subject_code as hash keys to myHashTable
-            //myHasTable will ensure uniqueness
-            if (myHashTable[j.subject_code] === undefined) {
-                myHashTable[j.subject_code] = i.student_id;
-            } else {
-                myHashTable[j.subject_code] += "," + i.student_id;
+        if (i.productname === prodname && i.productdetails.instock){
+            found = true;
+            for (p of i.productdetails.storesinventory){
+                if(p.suburb === suburb){
+                    subfound = true;
+                    return p.countavailable;
+                }
             }
         }
+        
     }
 
-    var bufferAsJson = [];
-
-//Now retain the unique students in each subject_course
-    for(key in myHashTable){
-        let unique = [...new Set(myHashTable[key].split(','))];
-        var subjectObj = {
-            subject_id : "",
-            studentArr : []
-        };
-        subjectObj.subject_id = key;
-        subjectObj.studentArr = unique;
-        bufferAsJson.push(subjectObj);
+    if(prodfound && subfound){
+    } else {
+        return "NRF"
     }
-//return
-    return bufferAsJson;
+    
 }
 
 
-let queryStudentDetails = function(dataset,courseId) {
-    const fs = require('fs');
-    let fileAsStr = fs.readFileSync(dataset).toString();
-    let fileAsJson = JSON.parse(fileAsStr);
-    myHashTable = {};
-
-//Step1: identify unique courses. Append all student names against the subject_code
-    for(i of fileAsJson){
-        for(j of i.class_details){
-            //Add subject_code as hash keys to myHashTable
-            //myHasTable will ensure uniqueness
-            if (myHashTable[j.subject_code] === undefined) {
-                myHashTable[j.subject_code] = i.student_id;
-            } else {
-                myHashTable[j.subject_code] += "," + i.student_id;
-            }
-        }
-    }
-
-    var bufferAsJson = [];
-
-//Now retain the unique students in each subject_course
-    for(key in myHashTable){
-        let unique = [...new Set(myHashTable[key].split(','))];
-        var subjectObj = {
-            subject_id : "",
-            studentArr : []
-        };
-        subjectObj.subject_id = key;
-        subjectObj.studentArr = unique;
-        bufferAsJson.push(subjectObj);
-    }
-    var nrf = true;
-    for(obj of bufferAsJson){
-        //console.log(obj);
-        if (obj.subject_id === courseId){
-            nrf = false;
-            return obj.studentArr
-        }
-    }
-    if(nrf){
-        return '{"Error":"No class found with the input courseId= ' + courseId + ' "}'
-    }
-
-}
-
-
-let queryCourses = function(dataset,courseId) {
-    const fs = require('fs');
-    let fileAsStr = fs.readFileSync(dataset).toString();
-    let fileAsJson = JSON.parse(fileAsStr);
-    myHashTable = {};
-
-    for(i of fileAsJson){
-        for(j of i.class_details){
-            //Add subject_code as hash keys to myHashTable
-            //myHasTable will ensure uniqueness
-            if (myHashTable[j.subject_code] === undefined) {
-                myHashTable[j.subject_code] = i.student_id;
-            } else {
-                myHashTable[j.subject_code] += "," + i.student_id;
-            }
-        }
-    }
-    var bufferAsJson = [];
-
-    for(key in myHashTable){
-        bufferAsJson.push(key);
-    }
-    return bufferAsJson;
-
-}
 
 
 module.exports = router;
